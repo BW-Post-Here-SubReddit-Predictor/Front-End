5366c22be290c845e637ec6ceba8b13a0b9551c2
Hui <jeffrey.x.hui@gmail.com>
2019-12-21 14:55:35 -0800

    created skeleton redux

--- src/index.js
+++ src/index.js
@@ -1,10 +1,33 @@
+//React
 import React from 'react';
 import ReactDOM from 'react-dom';
+//Styling
 import './index.css';
+//App
 import App from './App';
+//Router
+import { BrowserRouter as Router } from 'react-router-dom'
+//Redux
+import { createStore, compose, applyMiddleware } from 'redux'
+import { Provider } from 'react-redux'
+import thunk from 'redux-thunk'
+import logger from 'redux-logger'
+import { reducer } from './redux/reducers'
+//
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(reducer, composeEnhancers(applyMiddleware(thunk, logger)))
+
+
+
+ReactDOM.render(
+<Provider store={store}>
+<Router>
+<App />
+</Router>
+</Provider>
+, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
