77afbfa54e0073d502d700264da2bfa88be9de3c
robomantis19 <freefavors@gmail.com>
2019-12-21 19:03:49 -0500

    added home route

--- src/App.js
+++ src/App.js
@@ -1,24 +1,12 @@
 import React from 'react';
-import logo from './logo.svg';
+import { Route, Link, Switch } from 'react-router-dom';
 import './App.css';
-
+import PostHere from './components/PostHere'; 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Link to='/'>Home</Link>
+      <Route exact path='/' component={PostHere}/> 
     </div>
   );
 }
---
+++ src/components/PostHere.js
@@ -0,0 +1,14 @@
+import React from 'react'; 
+import users from '../library/PostHereUsers2'; 
+
+
+const PostHere = () => { 
+
+
+    return (
+        <div>
+            <h2>Post Here App</h2>
+        </div>
+    )
+}
+export default PostHere; 
