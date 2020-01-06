12ba9efa5fbdc1a17849cfeac52b91a2b30d3c83
Eian Carter <eian@bricleir.com>
2020-01-06 14:25:24 -0800

    added side nav into App.js

--- src/App.js
+++ src/App.js
@@ -8,13 +8,14 @@ import Home from './components/Home';
 import './App.css';
 //Components
 import PostHere from './components/PostHere'; 
-import SavedPosts from './components/SavedPosts'
-import ErrorPage from './components/ErrorPage'
+import SavedPosts from './components/SavedPosts';
+import ErrorPage from './components/ErrorPage';
+import SideNav from './components/SideNav';
 
 function App() {
   return (
     <div className="App">
-      <Link to='/Feed'>Feed</Link>
+      <SideNav />
       <Route exact path='/' component={Home} />
       <Route exact path='/Feed' component={PostHere}/> 
       <Route path='/Error' component={ErrorPage} />
