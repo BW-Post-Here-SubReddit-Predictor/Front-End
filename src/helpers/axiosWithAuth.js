192f5bbd37fe1f2bb8f6927bdcbb0396a561f743
Hui <jeffrey.x.hui@gmail.com>
2020-01-06 09:45:13 -0800

    login

--- src/helpers/axiosWithAuth.js
+++ src/helpers/axiosWithAuth.js
@@ -4,6 +4,7 @@ export const axiosWithAuth = () => {
   const token = localStorage.getItem('token')
 
   return axios.create({
+    baseURL: 'https://testingback.herokuapp.com/api',
     headers: {
       'Content-Type': 'application/json',
       'Authorization': `${token}`
