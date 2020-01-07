ab12ff08127f5d4765b7b0fe762c90a1133405ae
Eian Carter <eian@bricleir.com>
2020-01-07 13:54:14 -0800

    added persistence to posts

--- src/components/PostHere/PostCard.js
+++ src/components/PostHere/PostCard.js
@@ -1,13 +1,20 @@
-import React from 'react'
+import React from 'react';
+import { useHistory } from 'react-router-dom';
 
-import PostSubCard from './PostSubCard';
-import PostResponse from './PostResponse';
 
-const PostCard = () => {
+
+const PostCard = ({ item }) => {
+    const history = useHistory()
+
     return (
         <div>
-            <PostResponse />
-            <PostSubCard />
+            <div>{item.title}</div>
+            <div>{item.post_body}</div>
+            <div>{item.subreddits.map((item, index) => (<div key={index}>{item.name}</div>))}
+            </div>
+            <button>save</button>
+            <button>edit</button>
+            <button>delete</button>
         </div>
     )
 }
