4d1010cff00b8df6707cc31c84d35a67af9628b3
Eian Carter <eian@bricleir.com>
2020-01-08 13:44:57 -0800

    saved posts edit

--- src/components/SavedPosts.js
+++ src/components/SavedPosts.js
@@ -1,6 +1,5 @@
 //React
 import React, { useEffect, useState } from 'react'
-import axios from 'axios';
 //Components
 import PostCard from './PostHere/PostCard'
 //Actions
@@ -9,44 +8,18 @@ import { connect } from 'react-redux';
 
 const SavedPosts = props => {
   // get req to server to retrieve SAVED posts (with id)
+
   useEffect(() => {
     props.getUserPosts(localStorage.getItem('userId'))
-    // it's not working yet because we need a link to not have to refresh page
-    // axios
-    // .get(`/api/posts/${userID}/user`)
-    // .then(res => {
-    //   getUserPosts(res.data);
-    // })
-    // .catch(err => {
-    //   console.log(err);
-    // })
+  // it's not working yet because we need a link to not have to refresh page
   },[])
-
-  const [dummyArray, setDummyArray] = useState([
-    {
-      title: 'fake title',
-      post: 'fake news',
-      subreddits: [
-        {
-          'name': 'fake subreddit',
-          'proba': 0.1
-        },
-        {
-          'name': 'another fake subreddit',
-          'proba': 0.2
-        }
-      ],
-      id: 222,
-      userID: localStorage.getItem('userID')
-    }
-  ])
 
   // replace dummyArray with props.userPosts once backend data comes through
 
   return (
     <div>
-      {dummyArray.map((item, index) => (
-        <PostCard key={index} item={item} />
+      {props.userPosts.map((item, index) => (
+         <PostCard key={index} item={item} />
       ))}
     </div>
   )
