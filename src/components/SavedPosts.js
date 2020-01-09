eeda71d4e94015c66b76af7665d98f1c32636aa4
Eian Carter <eian@bricleir.com>
2020-01-09 11:52:20 -0800

    removed styled components from savedposts and moved into separate file

--- src/components/SavedPosts.js
+++ src/components/SavedPosts.js
@@ -5,6 +5,7 @@ import Styled from 'styled-components'
 import PostCard from './PostHere/PostCard'
 import LoginSpinner from './Home/LoginSpinner'
 import SearchSVG from './SavedPosts/SearchSVG'
+import { Form, Input, Button } from '../components/SavedPosts/SearchStyle'
 //Actions
 import { getAllPosts, getUserPosts } from '../redux/actions'
 import { connect } from 'react-redux' 
@@ -12,51 +13,6 @@ import { connect } from 'react-redux'
 const SavedPostsContainer = Styled.div`
     margin: 0 auto;
 `;
-
-//search styles start
-const Form = Styled.form`
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  box-shadow: 0 4px 8px rgba(0, 0, 0, 0.2);
-  background-color: white;
-  /* Change width of the form depending if the bar is opened or not */
-  width: ${props => (props.barOpened ? "15rem" : "1rem")};
-  /* If bar opened, normal cursor on the whole form. If closed, show pointer on the whole form so user knows he can click to open it */
-  cursor: ${props => (props.barOpened ? "auto" : "pointer")};
-  padding: 1rem;
-  height: 1rem;
-  border-radius: 2rem;
-  transition: width 300ms cubic-bezier(0.645, 0.045, 0.355, 1);
-`;
-const Input = Styled.input`
-  font-size: 14px;
-  line-height: 1;
-  background-color: transparent;
-  width: 100%;
-  margin-left: ${props => (props.barOpened ? "1rem" : "0rem")};
-  border: none;
-  color: black;
-  transition: margin 300ms cubic-bezier(0.645, 0.045, 0.355, 1);
-
-  &:focus,
-  &:active {
-    outline: none;
-  }
-  &::placeholder {
-    color: white;
-  }
-`;
-const Button = Styled.button`
-  line-height: 1;
-  pointer-events: ${props => (props.barOpened ? "auto" : "none")};
-  cursor: ${props => (props.barOpened ? "pointer" : "none")};
-  background-color: transparent;
-  border: none;
-  outline: none;
-  color: white;
-`;
-//search style end
 
 const SavedPosts = props => {
   // get req to server to retrieve SAVED posts (with id)
